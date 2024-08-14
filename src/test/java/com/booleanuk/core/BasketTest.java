ea1d30a5910e16f0a38e13ded33593fee72eb207
gremble0 <hermanstornes@gmail.com>
2024-08-14 13:11:00 +0200

    Add testAddBagel test case

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -4,5 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BasketTest {
-
+  @Test
+  public void testAddBagel() {
+    Basket basket = new Basket();
+    basket.add(Bagel.Plain);
+    Assertions.assertEquals(basket.size(), 1);
+    basket.add(Bagel.Rainbow);
+    Assertions.assertEquals(basket.size(), 2);
+  }
 }
