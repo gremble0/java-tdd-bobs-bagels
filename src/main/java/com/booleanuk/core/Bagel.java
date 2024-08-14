04c4db996049ccfbf41aa17f61a6a442553d4e9c
gremble0 <hermanstornes@gmail.com>
2024-08-14 13:15:09 +0200

    Implement Basket::add

---
+++ src/main/java/com/booleanuk/core/Bagel.java
@@ -0,0 +1,8 @@
+package com.booleanuk.core;
+
+public enum Bagel {
+  Plain,
+  Everything,
+  Egg,
+  Rainbow,
+}
--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,22 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
+  private int capacity = 5;
+  private List<Bagel> bagels = new ArrayList<>();
+
+  public int size() {
+    return bagels.size();
+  }
+
+  public boolean add(Bagel bagel) {
+    if (this.bagels.size() == this.capacity)
+      return false;
+
+    this.bagels.add(bagel);
+    return true;
+  }
 
 }
