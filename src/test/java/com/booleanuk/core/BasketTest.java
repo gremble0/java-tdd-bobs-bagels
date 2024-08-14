4d6619f28a7e4b8d0fe483a7ed5004e4353da998
gremble0 <hermanstornes@gmail.com>
2024-08-14 13:41:43 +0200

    Add tests for add to full and set capacity

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -14,6 +14,21 @@ class BasketTest {
   }
 
   @Test
+  public void testAddBagelToFullBasket() {
+    Basket basket = new Basket();
+    Assertions.assertEquals(basket.capacity, 5);
+
+    Assertions.assertTrue(basket.add(Bagel.Plain));
+    Assertions.assertTrue(basket.add(Bagel.Rainbow));
+    Assertions.assertTrue(basket.add(Bagel.Egg));
+    Assertions.assertTrue(basket.add(Bagel.Egg));
+    Assertions.assertTrue(basket.add(Bagel.Egg));
+
+    Assertions.assertFalse(basket.add(Bagel.Egg));
+    Assertions.assertEquals(basket.capacity, 5);
+  }
+
+  @Test
   public void testRemoveBagel() {
     Basket basket = new Basket();
     basket.add(Bagel.Plain);
@@ -21,5 +36,22 @@ class BasketTest {
 
     Assertions.assertTrue(basket.remove(Bagel.Plain));
     Assertions.assertFalse(basket.remove(Bagel.Everything));
+  }
+
+  @Test
+  public void testSetCapacity() {
+    Basket basket = new Basket();
+    Assertions.assertEquals(basket.capacity, 5);
+    basket.setCapacity(10);
+    Assertions.assertEquals(basket.capacity, 10);
+
+    Assertions.assertTrue(basket.add(Bagel.Plain));
+    Assertions.assertTrue(basket.add(Bagel.Rainbow));
+    Assertions.assertTrue(basket.add(Bagel.Egg));
+    Assertions.assertTrue(basket.add(Bagel.Egg));
+    Assertions.assertTrue(basket.add(Bagel.Egg));
+    Assertions.assertTrue(basket.add(Bagel.Rainbow));
+    Assertions.assertTrue(basket.add(Bagel.Everything));
+    Assertions.assertTrue(basket.add(Bagel.Egg));
   }
 }
